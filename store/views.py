3e19f3118f0e4c3cd62df565ea1f620a92c2c316
clinton <mogere.atuti@strathmore.edu>
2022-10-18 17:22:48 +0300

    Firstcommit

--- store/views.py
+++ store/views.py
@@ -1,9 +1,12 @@
+from itertools import product
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-	context = {}
-	return render(request, 'store/store.html', context)
+    products= Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
 	context = {}
