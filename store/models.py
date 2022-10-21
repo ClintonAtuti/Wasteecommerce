d78d0f51b41fdc908c1bc0e6172747a7843aadf5
clinton <mogere.atuti@strathmore.edu>
2022-10-21 10:27:06 +0300

    cart

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from email.headerregistry import Address
 from email.policy import default
 from enum import auto
+from itertools import product
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
@@ -41,14 +42,31 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    @property
+    def get_cart_items(self):
+        orderitems =self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+        
+    
 class OrderItem(models.Model):
-    customer = models.ForeignKey(Product, on_delete =models.SET_NULL, null= True)
+    product = models.ForeignKey(Product, on_delete =models.SET_NULL, null= True)
     order = models.ForeignKey(Order, on_delete =models.SET_NULL, null= True)
     quantity= models.IntegerField(default=0, null= True, blank=True)
     date_added =models.DateTimeField(auto_now_add= True)
     
+    @property
+    def get_total(self):
+        total =self.product.price * self.quantity
+        return total
+    
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(Product, on_delete =models.SET_NULL, null= True)
+    customer = models.ForeignKey(Customer, on_delete =models.SET_NULL, null= True)
     order = models.ForeignKey(Order, on_delete =models.SET_NULL, null= True)
     address = models.CharField(max_length=200, null= False)
     city = models.CharField(max_length=200, null= False)
